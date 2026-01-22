3bd675990359f783b6382ed680815cdfe33215fd
Rugers <2099387470@qq.com>
2026-01-23 00:13:18 +0800

    create the B

---
+++ round/1073div2/B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() { int n; }
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
