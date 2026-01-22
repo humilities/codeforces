8a15639d922d428fd80b51f6d8926e8402932ee8
Rugers <2099387470@qq.com>
2026-01-22 18:26:01 +0800

    the idea of queue, i think the time will exeed

--- round/1072div2/E.cpp
+++ round/1072div2/E.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+typedef long long ll;
+
+ll suba(int length) {
+  ll ret = 0;
+
+  for (int i = length; i > 0; i--) {
+    ret += length - (i - 1);
+  }
+
+  return ret;
+}
 
 void solve() {
   int n;
@@ -9,6 +20,24 @@ void solve() {
   vector<int> num(n);
   for (int i = 0; i < n; i++) {
     cin >> num[i];
+  }
+
+  vector<int> base(n - 1);
+  for (int i = 0; i < n - 1; i++) {
+    base[i] = abs(num[i + 1] - num[i]);
+  }
+
+  queue<int> op;
+  vector<ll> ret(n);
+  for (int i = 1; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (base[j] > i)
+        op.push(base[j]);
+
+      if (base[j + 1] < i) {
+        ret[i] += suba(op.size());
+      }
+    }
   }
 }
 
