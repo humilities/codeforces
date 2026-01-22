4cd4297d508a89b9788844090ffc9879c168f8a3
Rugers <2099387470@qq.com>
2026-01-22 17:59:06 +0800

    basic

--- round/1072div2/E.cpp
+++ round/1072div2/E.cpp
@@ -1,10 +1,27 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+
+  vector<int> num(n);
+  for (int i = 0; i < n; i++) {
+    cin >> num[i];
+  }
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
 
   return 0;
 }
