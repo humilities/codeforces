55c15b088d9d3dcb3e4df739eb8d7fefc2ccbd6a
Rugers <2099387470@qq.com>
2026-01-24 21:56:48 +0800

    the solution of B

--- round/1073div2/B.cpp
+++ round/1073div2/B.cpp
@@ -2,13 +2,35 @@
 
 using namespace std;
 
-void solve() { int n; }
+void solve() {
+  int n;
+  cin >> n;
+
+  vector<int> a(110, 0);
+  for (int i = 0; i < n; i++) {
+    int num;
+    cin >> num;
+
+    a[num]++;
+  }
+
+  if (a[0] == 0)
+    cout << "NO" << endl;
+  else if (a[1] > 0)
+    cout << "YES" << endl;
+  else
+    cout << (a[0] == 1 ? "YES" : "NO") << endl;
+
+  return;
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
   int t;
+  cin >> t;
+
   while (t--) {
     solve();
   }
