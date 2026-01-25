82ee18e2eca31780e71644a2dc753621681364fe
Rugers <2099387470@qq.com>
2026-01-26 00:21:26 +0800

    the sulotion of qo73div2

--- round/1076div3 /B.cpp
+++ round/1076div3 /B.cpp
@@ -4,60 +4,41 @@ using namespace std;
 
 void solve() {
   int n;
-  cin >> n;
+  if (!(cin >> n))
+    return;
 
-  vector<int> a(n);
-  map<int, int> hash;
-  for (int i = 0; i < n; i++) {
-    cin >> a[i];
-    map[a[i]] = i;
+  vector<int> p(n);
+  vector<int> pos(n + 1);
+
+  for (int i = 0; i < n; ++i) {
+    cin >> p[i];
+    pos[p[i]] = i;
   }
 
-  int l = 0, r = 0;
-  for (int i = n; i > 0; i--) {
-    if (a[n - i] == i) {
-      continue;
-    } else {
-      l = n - i;
-      r = hash[i];
+  for (int i = 0; i < n; ++i) {
+    int target_val = n - i;
 
+    if (p[i] != target_val) {
+      int r = pos[target_val];
+      reverse(p.begin() + i, p.begin() + r + 1);
       break;
     }
   }
 
-  if (l == 0 &&r == = 0) {
-    for (int i = 0; i < n; i++)
-      cout << a[i] << ' ';
-    cout << "/n" << endl;
-
-    return;
-  } else {
-    int j = r;
-    for (int i = l; i <= r; i++) {
-      int num = a[i];
-      a[i] = a[j];
-      a[j] = num;
-
-      j--;
-    }
-
-    for (int i = 0; i < n; i++)
-      cout << a[i] << ' ';
-    cout << "/n" << endl;
-
-    return;
+  for (int i = 0; i < n; ++i) {
+    cout << p[i] << (i == n - 1 ? "" : " ");
   }
+  cout << endl;
 }
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-
   int t;
-  cin >> t;
-
-  while (t--)
-    solve();
-
+  if (cin >> t) {
+    while (t--) {
+      solve();
+    }
+  }
   return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,13 +1,50 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main() 
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+void solve() {
+  int n, q;
+  cin >> n >> q;
 
-	cout<<"Hello world"<<endl;
+  vector<int> a(n), b(n);
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+  for (int i = 0; i < n; i++)
+    cin >> b[i];
 
-	return 0;
+  vector<long long> suf(n);
+  long long cur = 0;
+  for (int i = n - 1; i >= 0; i--) {
+    cur = max({(long long)a[i], (long long)b[i], cur});
+    suf[i] = cur;
+  }
+
+  vector<long long> pre(n + 1);
+  for (int i = 0; i < n; i++) {
+    pre[i + 1] = pre[i] + suf[i];
+  }
+
+  for (int i = 0; i < q; i++) {
+    int l, r;
+    cin >> l >> r;
+
+    if (q != n - 1)
+      cout << (pre[r] - pre[l - 1]) << " ";
+    else
+      cout << pre[r] - pre[l - 1] << "\n";
+  }
+
+  return;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin >> t;
+
+  while (t--)
+    solve();
+  return 0;
 }
