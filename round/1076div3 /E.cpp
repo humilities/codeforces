5e2abb944e0e20ee4af9fa6a16a74101d5a74a4c
humilities <2099387470@qq.com>
2026-01-27 21:16:46 +0800

    solve the 1073div3 E in dp and sovle a bugdp named writing code

--- round/1076div3 /E.cpp
+++ round/1076div3 /E.cpp
@@ -1,14 +1,33 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+const int maxn = 1000012;
+
+int a[maxn], dp[maxn];
 
 void solve() {
   int n;
   cin >> n;
 
-  vector<int> a(n);
-  for (int i = 0; i < n; i++)
+  for (int i = 1; i <= n; i++)
+    dp[i] = 1e9;
+
+  for (int i = 1; i <= n; i++) {
     cin >> a[i];
+    dp[a[i]] = 1;
+  }
+
+  for (int i = 1; i <= n; i++)
+    for (int j = i; j <= n; j += i) {
+      dp[j] = min(dp[j], dp[i] + dp[j / i]);
+    }
+
+  for (int i = 1; i <= n; i++) {
+    if (dp[i] == 1e9)
+      dp[i] = -1;
+
+    cout << dp[i] << " \n"[i == n];
+  }
 }
 
 int main() {
