05658434316a5c06d5c67dc3f928e091a7993567
humilities <2099387470@qq.com>
2026-02-06 16:27:18 +0800

    use the math and dp

--- Yet_another_minmize_problem.cpp
+++ Yet_another_minmize_problem.cpp
@@ -2,7 +2,51 @@
 
 using namespace std;
 
-void solve() { int n; }
+void solve() {
+
+  int n;
+  cin >> n;
+
+  vector<int> a(n), b(n);
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+  for (int i = 0; i < n; i++)
+    cin >> b[i];
+
+  if (n == 1) {
+    cout << 0 << endl;
+    return;
+  }
+
+  int sum = 0;
+  int tol = 0;
+  for (int i = 0; i < n; i++) {
+    sum += a[i] * a[i] + b[i] * b[i];
+    tol += a[i] + b[i];
+  }
+
+  bitset<100005> dp;
+  dp[0] = 1;
+
+  for (int i = 0; i < n; i++) {
+    dp = (dp << a[i]) | (dp << b[i]);
+  }
+
+  int val = -1;
+
+  for (int i = 0; i < tol; i++) {
+    if (dp[i]) {
+      int a = tol - i;
+      int num = (n - 2) * sum + i * i + a * a;
+
+      if (val == -1 || num < val) {
+        val = num;
+      }
+    }
+  }
+
+  cout << val << endl;
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
