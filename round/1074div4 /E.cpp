d11dae68a34f2a2a2494ca7e0a918dc3b2be7951
humilities <2099387470@qq.com>
2026-02-09 17:53:54 +0800

    solve the 1074div4 E

--- round/1074div4 /E.cpp
+++ round/1074div4 /E.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -11,8 +12,50 @@ void solve() {
 
   for (int i = 0; i < n; i++)
     cin >> a[i];
-  for (int i = 0; i < n; i++)
+  for (int i = 0; i < m; i++)
     cin >> b[i];
+
+  string str;
+  cin >> str;
+
+  vector<bool> dead(n);
+  map<int, vector<int>> se;
+
+  sort(b.begin(), b.end());
+
+  for (int i = 0; i < n; i++) {
+    if (b[0] < a[i]) {
+      int left = a[i] - (*(lower_bound(b.begin(), b.end(), a[i]) - 1));
+      se[-left].push_back(i);
+    }
+    if (b[m - 1] > a[i]) {
+      int right = *(lower_bound(b.begin(), b.end(), a[i])) - a[i];
+      se[right].push_back(i);
+    }
+  }
+
+  int ret = n;
+  int pos = 0;
+
+  for (int i = 0; i < str.length(); i++) {
+    if (str[i] == 'L')
+      pos--;
+    else
+      pos++;
+
+    for (int j : se[pos]) {
+      if (dead[j])
+        continue;
+
+      dead[j] = true;
+      ret--;
+    }
+    se[pos].clear();
+
+    cout << ret << " ";
+  }
+
+  cout << endl;
 }
 
 int main() {
