28ba4000353670827fad13b290e3f8d1d71aa7a0
humilities <2099387470@qq.com>
2026-03-17 20:48:48 +0800

    river

--- atcoder/ABC/449/C.cpp
+++ atcoder/ABC/449/C.cpp
@@ -6,5 +6,26 @@ int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
+  int n, l, r;
+  cin >> n >> l >> r;
+  r++;
+
+  string str;
+  cin >> str;
+
+  int cnt[26] = {0};
+  long long ans = 0;
+
+  for (int i = 0; i < n; i++) {
+    if (i >= l)
+      cnt[str[i - l] - 'a']--;
+    if (i >= r)
+      cnt[str[i - r] - 'a']++;
+
+    ans += cnt[str[i] - 'a'];
+  }
+
+  cout << ans << endl;
+
   return 0;
 }
---
+++ round/187div2/C.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+void solve() {
+  ll s, m;
+  cin >> s >> m;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin >> t;
+
+  while (t--)
+    solve();
+
+  return 0;
+}
