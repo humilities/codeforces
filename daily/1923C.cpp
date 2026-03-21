2cf998914a98399198cece81c8510dcfc8ffea6d
humilities <2099387470@qq.com>
2026-03-21 16:19:34 +0800

    we meet ...

--- daily/1923C.cpp
+++ daily/1923C.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+typedef long long ll;
 
 void solve() {
   int n, q;
@@ -10,9 +11,45 @@ void solve() {
   for (int i = 0; i < n; i++)
     cin >> a[i];
 
+  vector<ll> pre(n + 1, 0);
+  for (int i = 1; i <= n; i++) {
+    pre[i] = pre[i - 1] + a[i - 1];
+  }
+
+  vector<int> cnt(n + 1);
+  cnt[0] = 0;
+  for (int i = 1; i <= n; i++) {
+    if (a[i - 1] == 1) {
+      cnt[i] = cnt[i - 1] + 1;
+    } else
+      cnt[i] = cnt[i - 1];
+  }
+
   while (q--) {
     int l, r;
     cin >> l >> r;
+
+    l--, r--;
+
+    if (l == r) {
+      cout << "NO" << endl;
+      continue;
+    }
+
+    ll sum = pre[r + 1] - pre[l];
+    int tol = cnt[r + 1] - cnt[l];
+    ll len = r - l + 1;
+
+    // int temp = tol * 2;
+
+    // cout << tol << " " << len << endl;
+
+    if (tol + len > sum) {
+      cout << "NO" << endl;
+      continue;
+    }
+
+    cout << "YES" << endl;
   }
 }
 
--- round/1083div2/C.cpp
+++ round/1083div2/C.cpp
@@ -2,18 +2,73 @@
 
 using namespace std;
 
+const int MAXN = 1e6 + 10, inf = ~0U >> 2;
+int n, vis[MAXN];
+vector<int> a[8010];
+int chg[8010];
+
+void init(vector<int> &now) {
+  for (auto &v : now)
+    cin >> v, vis[v] = 0;
+
+  reverse(begin(now), end(now));
+  vector<int> nw;
+
+  for (auto v : now)
+    if (!vis[v])
+      nw.emplace_back(v), vis[v] = 1;
+  for (auto v : nw)
+    vis[v] = 0;
+
+  now = nw;
+}
+
+void change(vector<int> &now) {
+  if (now.size() == 1 && now[0] == inf)
+    return;
+
+  vector<int> nw;
+  for (auto v : now)
+    if (!vis[v])
+      nw.emplace_back(v);
+
+  now = nw;
+}
+
 void solve() {
-  int n;
   cin >> n;
 
-  vector<vector<int>> b(n);
-  for (int i = 0; i < n; i++) {
-    int l;
-    cin >> l;
+  for (int i = 1; i <= n; i++) {
+    int q = 0;
+    cin >> q;
 
-    for (int j = 0; j < l; j++)
-      cin >> b[i][j];
+    a[i].resize(q);
+    init(a[i]);
+    chg[i] = 0;
   }
+
+  for (int i = 1; i <= n; i++) {
+    int id = 1;
+
+    for (int j = 1; j <= n; j++) {
+      if (!chg[j] && a[j] < a[id])
+        id = j;
+    }
+
+    auto &now = a[id];
+    for (auto v : now)
+      cout << v << " ", vis[v] = 1;
+
+    now = vector<int>{inf};
+    chg[id] = 1;
+
+    for (int j = 1; j <= n; j++) {
+      if (!chg[j])
+        change(a[j]);
+    }
+  }
+
+  cout << "\n";
 }
 
 int main() {
