d1edc499d6031a1895b16ef8960a87f4d8dc507e
humilities <2099387470@qq.com>
2026-04-03 10:41:42 +0800

    linziiii

--- xtuvj/basicweek3/A.cpp
+++ xtuvj/basicweek3/A.cpp
@@ -10,6 +10,22 @@ void solve() {
   vector<int> a(n);
   for (int i = 0; i < n; i++)
     cin >> a[i];
+
+  int mn = 15;
+
+  for (int k = 0; k < n; k++) {
+
+    for (int i = 0; i <= 15; i++) {
+      for (int j = 0; j <= 15; j++)
+        if (((a[k] + i) << j) % 32768 == 0)
+          mn = min(mn, i + j);
+    }
+
+    cout << mn << " ";
+    mn = 15;
+  }
+
+  cout << "\n";
 }
 
 int main() {
