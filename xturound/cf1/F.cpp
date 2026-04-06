43958fd9f03c5e4deda4e9db193bab515cfb75aa
humilities <2099387470@qq.com>
2026-04-06 22:45:56 +0800

    lixinyue

--- xturound/cf1/F.cpp
+++ xturound/cf1/F.cpp
@@ -1,46 +1,134 @@
+// #include <bits/stdc++.h>
+//
+// using namespace std;
+// using ll = long long;
+//
+// struct node {
+//   ll k;
+//   ll high;
+//   ll de;
+//
+//   bool operator<(const node &other) const { return de < other.de; }
+// };
+//
+// void solve() {
+//   int n;
+//   ll x;
+//   cin >> n >> x;
+//
+//   vector<ll> k(n);
+//   vector<ll> h(n);
+//
+//   ll sum = 0;
+//   for (int i = 0; i < n; i++)
+//     cin >> k[i] >> h[i], sum += k[i] * pow(h[i], 2);
+//
+//   priority_queue<node> p;
+//   for (int i = 0; i < n; i++)
+//     p.push({k[i], h[i], k[i] * (2 * h[i] - 1)});
+//
+//   int cnt = 0;
+//   while (sum > x) {
+//     node n = p.top();
+//     p.pop();
+//
+//     sum -= n.de;
+//     n.high--;
+//
+//     if (n.high)
+//       p.push({n.k, n.high, n.k * (n.high * 2 - 1)});
+//     cnt++;
+//   }
+//
+//   cout << cnt << "\n";
+// }
+//
+// int main() {
+//   ios_base::sync_with_stdio(false);
+//   cin.tie(NULL);
+//
+//   int t;
+//   cin >> t;
+//
+//   while (t--)
+//     solve();
+//
+//   return 0;
+// }
+//
 #include <bits/stdc++.h>
 
 using namespace std;
 using ll = long long;
+using i128 = __int128;
 
 struct node {
-  ll k;
-  ll high;
-  ll de;
-
-  bool operator<(const node &other) const { return de < other.de; }
+  ll k, h;
 };
 
-void solve() {
-  int n;
-  ll x;
-  cin >> n >> x;
+ll n, x;
+vector<node> tow;
 
-  vector<ll> k(n);
-  vector<ll> h(n);
+pair<i128, ll> che(ll mid) {
+  i128 sum = 0;
+  ll att = 0;
 
-  ll sum = 0;
-  for (int i = 0; i < n; i++)
-    cin >> k[i] >> h[i], sum += k[i] * pow(h[i], 2);
+  for (int i = 0; i < n; i++) {
+    ll hp;
 
-  priority_queue<node> p;
-  for (int i = 0; i < n; i++)
-    p.push({k[i], h[i], k[i] * (2 * h[i] - 1)});
+    if ((i128)tow[i].k * (2 * tow[i].h - 1) <= mid)
+      hp = tow[i].h;
+    else {
+      hp = hp = (mid / tow[i].k + 1) / 2;
+      hp = min(tow[i].h, max(0LL, hp));
+    }
 
-  int cnt = 0;
-  while (sum > x) {
-    node n = p.top();
-    p.pop();
-
-    sum -= n.de;
-    n.high--;
-
-    if (n.high)
-      p.push({n.k, n.high, n.k * (n.high * 2 - 1)});
-    cnt++;
+    sum += (i128)tow[i].k * hp * hp;
+    att += (tow[i].h - hp);
   }
 
-  cout << cnt << "\n";
+  return {sum, att};
+}
+
+void solve() {
+  tow.clear();
+
+  cin >> n >> x;
+  tow.resize(n);
+
+  i128 sum = 0;
+  for (int i = 0; i < n; i++) {
+    cin >> tow[i].k >> tow[i].h;
+    sum += (i128)tow[i].k * tow[i].h * tow[i].h;
+  }
+
+  if (sum <= (i128)x) {
+    cout << 0 << "\n";
+    return;
+  }
+
+  ll l = 0, best = 1;
+  i128 r = 4e18;
+  while (l <= r) {
+    ll mid = l + (r - l) / 2;
+
+    if (che(mid).first <= (i128)x) {
+      best = mid;
+      l = mid + 1;
+    } else
+      r = mid - 1;
+  }
+
+  auto ans = che(best);
+  i128 s = ans.first;
+  ll cnt = ans.second;
+
+  if (best < 2e18) {
+    ll re = (ll)((x - s) / (i128)(best + 1));
+    cout << max(0LL, cnt - re) << "\n";
+  } else {
+    cout << cnt << "\n";
+  }
 }
 
 int main() {
--- xtuvj/higherweek2/A.cpp
+++ xtuvj/higherweek2/A.cpp
@@ -2,9 +2,34 @@
 
 using namespace std;
 
+int dp[26][26];
+
 void solve() {
   int n;
   cin >> n;
+
+  vector<string> a(n);
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+
+  for (int i = 0; i < n; i++) {
+    string s = a[i];
+    int len = s.length();
+    int l = s[0] - 'a';
+    int r = s[len - 1] - 'a';
+
+    for (int j = 0; j < 26; j++)
+      if (dp[j][l])
+        dp[j][r] = max(dp[j][l] + len, dp[j][r]);
+
+    dp[l][r] = max(dp[l][r], len);
+  }
+
+  int ma = 0;
+  for (int i = 0; i < 26; i++)
+    ma = max(ma, dp[i][i]);
+
+  cout << ma << "\n";
 }
 int main() {
   ios_base::sync_with_stdio(false);
