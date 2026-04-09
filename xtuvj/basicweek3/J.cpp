784221cf08db4e1758534d3537d31eeb24462c92
humilities <2099387470@qq.com>
2026-04-09 11:21:00 +0800

    linziiiii

--- xtuvj/basicweek3/J.cpp
+++ xtuvj/basicweek3/J.cpp
@@ -9,7 +9,43 @@ void solve() {
   cin >> n >> k;
 
   vector<int> a(n);
-  lin(i, 0, n) cin >> a[i];
+  map<int, int> ma;
+  lin(i, 0, n) cin >> a[i], ma[a[i]]++;
+
+  vector<int> b;
+  for (auto const &[val, seq] : ma)
+    if (seq >= k)
+      b.push_back(val); //, cout << val << " ";
+
+  // cout << "\n";
+
+  if (b.empty()) {
+    cout << "-1" << "\n";
+    return;
+  }
+
+  int l = b[0], r = b[0];
+  int curl = b[0];
+  int ans = -1;
+
+  for (int i = 1; i < b.size(); i++) {
+    if (b[i] == b[i - 1] + 1) {
+      if (b[i] - curl > ans) {
+        ans = b[i] - curl;
+        l = curl;
+        r = b[i];
+
+        // cout << b[i] << "\n";
+      }
+    } else
+      curl = b[i];
+  }
+
+  if (ans == -1) {
+    cout << b[0] << " " << b[0] << "\n";
+    return;
+  }
+  cout << l << " " << r << "\n";
 }
 
 int main() {
