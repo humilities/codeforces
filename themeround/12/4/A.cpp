a74602d1c7a3a9c53ac8e8f7309075ea3a85e717
humilities <2099387470@qq.com>
2026-04-12 22:34:23 +0800

    linziiiii

--- themeround/12/4/A.cpp
+++ themeround/12/4/A.cpp
@@ -4,7 +4,38 @@
 using namespace std;
 using ll = long long;
 
-void solve() {}
+void solve() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+
+  int m1 = max(a, b);
+  int n1 = min(a, b);
+
+  int rem = ceil((double)m1 / (n1 + 1));
+  if (rem >= 3) {
+    cout << "NO" << "\n";
+    return;
+  } else {
+    c = c - a;
+    d = d - b;
+
+    // cout << c << " " << d << "\n";
+
+    m1 = max(c, d);
+    n1 = min(c, d);
+
+    rem = ceil((double)m1 / (n1 + 1));
+
+    // cout << rem << "\n";
+
+    if (rem >= 3) {
+      cout << "NO" << "\n";
+      return;
+    }
+
+    cout << "YES" << "\n";
+  }
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
