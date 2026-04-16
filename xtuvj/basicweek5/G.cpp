ca74951b39ef0a9291bf5d4d6572a1e01651dd1c
humilities <2099387470@qq.com>
2026-04-16 20:41:37 +0800

    linziiiii

--- xtuvj/basicweek5/G.cpp
+++ xtuvj/basicweek5/G.cpp
@@ -4,7 +4,28 @@
 using namespace std;
 using ll = long long;
 
-void solve() {}
+void solve() {
+  int n;
+  cin >> n;
+
+  vector<int> a(n);
+  lin(i, 0, n) cin >> a[i];
+
+  map<int, int> ma;
+  lin(i, 0, n) ma[a[i]]++;
+
+  int c1 = 0;
+  int c2 = 0;
+
+  for (auto [node, val] : ma) {
+    if (val == 1)
+      c1++;
+    else
+      c2++;
+  }
+
+  cout << c2 + ceil((double((double)c1 / 2))) << "\n";
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
