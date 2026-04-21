9778b987b744e2ce8e80a132f87401bc217ea7e3
humilities <2099387470@qq.com>
2026-04-21 16:36:47 +0800

    solve the G of xtuoj with dfs in test.cpp

--- test.cpp
+++ test.cpp
@@ -4,31 +4,50 @@
 using namespace std;
 using ll = long long;
 
-void solve() {
-    int n;
-    cin>>n;
-
-    vector<int> a(n);
-    lin(i,0,n) cin>>a[i];
-
+int f(string& s,int pos){
     int cnt=0;
-    lin(i,0,n) if(a[i]&1) cnt++;
 
-    //cout<<cnt<<" ";
-    
-    if(!(cnt&1)) cout<<"Yes"<<"\n";
-    else cout<<"No"<<"\n";
+    lin(i,pos,s.length()){
+        if(s[i]=='(') cnt++;
+        else if(s[i]==')') cnt--;
+
+        if(cnt==0) return i;
+    }
+
+    return -1;
+}
+
+ll solve(string s) {
+    if(s.length()==1) return s[0]-'0';
+    //if(s.length()==3 && s[0]=='(' && s[2]==')') return s[1]-'0';
+
+    ll sum=0;
+    int idx=0;
+    while(idx<s.length()){
+        if(s[idx]=='('){
+            int r=f(s,idx);
+            sum+=solve(s.substr(idx+1,r-idx-1));
+
+            idx=r+1;
+        }else if(isdigit(s[idx]))
+            sum+=s[idx]-'0',idx++;
+        else idx++;
+    }
+
+    return sum*sum;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t;
-    cin >> t;
+    //int t;
+    //cin >> t;
 
-    while (t--)
-        solve();
+    //while (t--)
+    string s;
+    while(cin>>s)
+        cout<<solve(s)<<"\n";
 
     return 0;
 }
