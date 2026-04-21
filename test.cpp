5ccb3769882de571926ea290418e3b5d62f36be9
humilities <2099387470@qq.com>
2026-04-21 15:23:25 +0800

    solve H of xtuoj with math in test.cpp

--- test.cpp
+++ test.cpp
@@ -1,114 +1,33 @@
-// #include <bits/stdc++.h>
-
-// #define lin(i, a, b) for (int i = (a); i < (b); i++)
-// using namespace std;
-// using ll = long long;
-
-// const int mn=200005;
-// vector<int> ve(mn);
-
-// void solve() {
-//     string s;
-//     int cnt=0;
-
-//     while(cin>>s){
-//         if(s=="push"){
-//             int x;
-//             cin>>x;
-
-//             ve.push_back(x);
-//             cnt++;
-//         }else{
-//             int ans=0;
-//             int mid=(cnt+1)/2;
-//             cnt--;
-
-//             int idx=0;
-//             int i=0;
-//             while(idx<=mid){
-//                 if(ve[i]==0) {i++; continue;}
-//                 else{
-//                     ans=ve[i];
-
-//                     ve[i]=0;
-//                     idx++;
-//                     i++;
-//                 }
-//             }
-
-//             cout<<ans<<"\n";
-//         }
-//     }
-// }
-
-// int main() {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(NULL);
-
-//     //int t;
-//     //cin >> t;
-
-//     //while (t--)
-//         solve();
-
-//     return 0;
-// }
-
 #include <bits/stdc++.h>
 
 #define lin(i, a, b) for (int i = (a); i < (b); i++)
 using namespace std;
 using ll = long long;
 
-priority_queue<int> l;
-priority_queue<int,vector<int>,greater<int>> r;
-
-void ba(int cnt){
-    int mid=(cnt+1)/2;
-
-    while(l.size() > mid){
-        r.push(l.top());
-        l.pop();
-    }
-
-    while(l.size() < mid){
-        l.push(r.top());
-        r.pop();
-    }
-}
-
 void solve() {
-    string s;
+    int n;
+    cin>>n;
+
+    vector<int> a(n);
+    lin(i,0,n) cin>>a[i];
+
     int cnt=0;
+    lin(i,0,n) if(a[i]&1) cnt++;
 
-    while(cin>>s){
-        if(s=="push"){
-            int x;
-            cin>>x;
-            cnt++;
-
-            if(l.empty() || x<=l.top()) l.push(x);
-            else r.push(x);
-
-            ba(cnt);
-        }else{
-            cout<<l.top()<<"\n";
-
-            l.pop();
-            cnt--;
-            ba(cnt);
-        }
-    }
+    //cout<<cnt<<" ";
+    
+    if(!(cnt&1)) cout<<"Yes"<<"\n";
+    else cout<<"No"<<"\n";
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    //int t;
-    //cin >> t;
+    int t;
+    cin >> t;
 
-    //while (t--)
+    while (t--)
         solve();
 
     return 0;
