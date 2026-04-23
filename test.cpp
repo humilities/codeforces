8ae954bd9b4c5411d11f6b9b91d55adedf3aceba
humilities <2099387470@qq.com>
2026-04-23 10:29:26 +0800

    solve I of xtuoj with bfs in test.cpp

--- test.cpp
+++ test.cpp
@@ -4,50 +4,44 @@
 using namespace std;
 using ll = long long;
 
-int f(string& s,int pos){
-    int cnt=0;
+const int mn=200005;
+int dis[mn];
 
-    lin(i,pos,s.length()){
-        if(s[i]=='(') cnt++;
-        else if(s[i]==')') cnt--;
+void solve() {
+    int x,y;
+    cin>>x>>y;
 
-        if(cnt==0) return i;
+    memset(dis,-1,sizeof(dis));
+    queue<int> q;
+    q.push(x);
+    dis[x]=0;
+
+    while(!q.empty()){
+        int cur=q.front();
+        q.pop();
+
+        int ne[4]={cur*2,cur+1,cur/2,cur-1};
+        lin(i,0,4){
+            int num=ne[i];
+
+            if(num==y)  {cout<<dis[cur]+1<<"\n"; return;}
+            if(num>=0 && num<mn && dis[num]==-1){
+                dis[num]=dis[cur]+1;
+                q.push(num);
+            }
+        }
     }
-
-    return -1;
-}
-
-ll solve(string s) {
-    if(s.length()==1) return s[0]-'0';
-    //if(s.length()==3 && s[0]=='(' && s[2]==')') return s[1]-'0';
-
-    ll sum=0;
-    int idx=0;
-    while(idx<s.length()){
-        if(s[idx]=='('){
-            int r=f(s,idx);
-            sum+=solve(s.substr(idx+1,r-idx-1));
-
-            idx=r+1;
-        }else if(isdigit(s[idx]))
-            sum+=s[idx]-'0',idx++;
-        else idx++;
-    }
-
-    return sum*sum;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    //int t;
-    //cin >> t;
+    int t;
+    cin >> t;
 
-    //while (t--)
-    string s;
-    while(cin>>s)
-        cout<<solve(s)<<"\n";
+    while (t--)
+        solve();
 
     return 0;
 }
