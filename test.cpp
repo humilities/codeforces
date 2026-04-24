a94ee5a445908725441b07f14062f6760f49c277
humilities <2099387470@qq.com>
2026-04-24 18:33:50 +0800

    solve J of xtuoj with bfs in test.cpp

--- test.cpp
+++ test.cpp
@@ -4,22 +4,56 @@
 using namespace std;
 using ll = long long;
 
-void solve() {
-    int n,k;
-    cin>>n>>k;
+const int mn = 10005;
+vector<int> adj[mn];
+int val[mn];
+int n;
 
-    vector<int> a(n);
-    lin(i,0,n) cin>>a[i];
+int dfs(int u, int p, int ops, int tar) {
+    int cur = (val[u] + ops) % 2;
+    int res = 0;
+    int num = ops;
+
+    if (cur != tar) {
+        res = 1;
+        num = ops + 1;
+    }
+
+    for (int v : adj[u])
+        if (v != p) 
+            res += dfs(v, u, num, tar);
+
+    return res;
+}
+
+void solve() {
+    cin >> n;
+    lin(i,1,n+1) adj[i].clear();
+    
+    lin(i,0,n-1) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    
+    lin(i,1,n+1)
+        cin >> val[i];
+
+    int res0 = dfs(1, 0, 0, 0);
+    int res1 = dfs(1, 0, 0, 1);
+    
+    cout << min(res0, res1) << "\n";
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    //int t;
-    //cin >> t;
+    int t;
+    cin >> t;
 
-    //while (t--)
+    while (t--)
         solve();
 
     return 0;
