4b085d927ec6e9a0c0f25337ad91be960144f8d8
humilities <2099387470@qq.com>
2026-04-24 19:25:59 +0800

    solve M of xtuoj with st in test.cpp

--- test.cpp
+++ test.cpp
@@ -4,18 +4,38 @@
 using namespace std;
 using ll = long long;
 
+const int mn=100005;
+const int lg=20;
+
+int st[mn][lg];
+int g[mn];
+
+void pre(int n,const vector<int>& a){
+    g[1]=0;
+    lin(i,2,n+1) g[i]=g[i/2]+1;
+
+    lin(i,1,n+1) st[i][0]=a[i-1];
+
+    lin(j,1,lg)
+        for(int i=1;i+(1<<j)-1<=n;i++) st[i][j]=st[i][j-1] | st[i+(1<<(j-1))][j-1];
+}
+
+int que(int l,int r){
+    int mid=g[r-l+1];
+    return st[l][mid] | st[r-(1<<mid)+1][mid];
+}
+
 void solve() {
-    int n;
-    cin>>n;
+    int n,k;
+    cin>>n>>k;
 
-    vector<int> a(3*n);
-    lin(i,0,3*n) cin>>a[i];
+    vector<int> a(n);
+    lin(i,0,n) cin>>a[i];
 
-    sort(a.begin(),a.end());
+    pre(n,a);
+    int ans=0;
 
-    ll ans=0;
-    for(int i=0;i<3*n;i+=3)
-        ans+=((a[i]-a[i+1])*(a[i]-a[i+1])+(a[i+1]-a[i+2])*(a[i+1]-a[i+2]));
+    for(int i=1;i<=n-k+1;i++) ans=max(ans,que(i,i+k-1));
 
     cout<<ans<<"\n";
 }
