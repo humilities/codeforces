4c9243f248984066299299c4fc59b4a54c098da7
humilities <2099387470@qq.com>
2026-04-28 17:06:57 +0800

    linziiiii

--- xtuvj/basicweek6/d.cpp
+++ xtuvj/basicweek6/d.cpp
@@ -5,18 +5,41 @@ using namespace std;
 using ll = long long;
 
 void solve() {
-  // this is a test.
+  int m, s, t;
+  cin >> m >> s >> t;
+
+  vector<int> dp(t + 1, 0);
+  lin(i, 1, t + 1) {
+    if (m >= 10)
+      dp[i] = dp[i - 1] + 60, m -= 10;
+    else
+      dp[i] = dp[i - 1], m += 4;
+  }
+
+  lin(i, 1, t + 1) {
+    dp[i] = max(dp[i], dp[i - 1] + 17);
+
+    if (dp[i] >= s) {
+      cout << "Yes" << "\n";
+      cout << i;
+
+      return;
+    }
+  }
+
+  cout << "No" << "\n";
+  cout << dp[t];
 }
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-  int t;
-  cin >> t;
+  // int t;
+  // cin >> t;
 
-  while (t--)
-    solve();
+  // while (t--)
+  solve();
 
   return 0;
 }
