b838d4892bab9d558e70cc77a9e06741d8e8f071
humilities <2099387470@qq.com>
2026-04-30 16:17:57 +0800

    linziiiii

--- round/1095div2/D.cpp
+++ round/1095div2/D.cpp
@@ -4,51 +4,40 @@
 using namespace std;
 using ll = long long;
 
-int cnt[1000005];
-
 void solve() {
     int n;
     cin>>n;
 
-    vector<int> a(n);
-    int mn = 0;
+    multiset<int> a;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        a.insert(x);
+    }
 
-    lin(i, 0, n) {
-        cin >> a[i];
-        if (a[i] < 1000005) {
-            cnt[a[i]]++;
-            mn = max(mn, a[i]);
+    int idx = 0; 
+    while (true) {
+        auto it = a.find(idx);
+        
+        if (it != a.end()) {
+            a.erase(it);
+            idx++;
+        }
+        else if (!a.empty()) {
+            auto it = prev(a.end());
+            if (*it >= 2 * idx + 1) {
+                a.erase(it);
+                idx++;
+            } else {
+                break;
+            }
+        } 
+        else {
+            break;
         }
     }
 
-    multiset<int> b;
-    int idx = 0;
-
-    while (cnt[idx]) {
-        lin(j, 0, cnt[idx] - 1) b.insert(idx);
-        idx++;
-    }
-
-    lin(i, idx, mn + 1)
-        lin(j, 0, cnt[i]) b.insert(i);
-
-    while (true) {
-        auto it = b.lower_bound(2 * idx);
-
-        if (it != b.end()) {
-            b.erase(it);
-            idx++;
-
-            while (cnt[idx]) {
-                lin(j, 0, cnt[idx] - 1) b.insert(idx);
-                idx++;
-            }
-        } else break;
-    }
-
-    cout <<idx<< "\n";
-
-    lin(i, 0, mn + 1) cnt[i] = 0;
+    cout << idx << "\n";
 }
 
 int main() {
