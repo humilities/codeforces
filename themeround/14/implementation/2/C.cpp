25671d583fb39518d9f8b2cead357649ff6942d7
humilities <2099387470@qq.com>
2026-05-03 22:16:20 +0800

    linziiiii

---
+++ themeround/14/implementation/2/C.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define lin(i, a, b) for (int i = (a); i < (b); i++)
+using namespace std;
+using ll = long long;
+
+void solve() {
+  int n, m, k;
+  cin >> n >> m >> k;
+
+  string s;
+  cin >> s;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin >> t;
+
+  while (t--)
+    solve();
+
+  return 0;
+}
