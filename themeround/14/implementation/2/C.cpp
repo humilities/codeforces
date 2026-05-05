f5621563f67f940a22996eafbe2412e828ff58be
humilities <2099387470@qq.com>
2026-05-05 17:08:08 +0800

    linziiiii

--- themeround/14/implementation/2/C.cpp
+++ themeround/14/implementation/2/C.cpp
@@ -10,6 +10,55 @@ void solve() {
 
   string s;
   cin >> s;
+
+  int cur = 0;
+  int dis = 0;
+  bool flag = true;
+
+  while (cur <= n) {
+    if (cur == 0 || s[cur - 1] == 'L') {
+      int nex = -1;
+
+      for (int i = min(n + 1, cur + m); i > cur; i--)
+        if (i == (n + 1) || s[i - 1] == 'L') {
+          nex = i;
+          break;
+        }
+
+      if (nex == -1) {
+        for (int i = min(n + 1, cur + m); i > cur; i--)
+          if (i == (n + 1) || s[i - 1] == 'W') {
+            nex = i;
+            break;
+          }
+      }
+
+      if (nex == -1) {
+        flag = false;
+        break;
+      }
+
+      cur = nex;
+    } else if (s[cur - 1] == 'W') {
+      cur++;
+      dis++;
+
+      if (dis > k) {
+        flag = false;
+        break;
+      }
+
+      if (cur <= n && s[cur - 1] == 'C') {
+        flag = false;
+        break;
+      }
+    }
+  }
+
+  if (flag)
+    cout << "YES" << "\n";
+  else
+    cout << "NO" << "\n";
 }
 
 int main() {
--- themeround/14/implementation/2/D.cpp
+++ themeround/14/implementation/2/D.cpp
@@ -4,7 +4,33 @@
 using namespace std;
 using ll = long long;
 
-void solve() {}
+void solve() {
+  int n, k;
+  cin >> n >> k;
+
+  vector<int> b(n);
+  lin(i, 0, n) cin >> b[i];
+
+  int cur = n - 1;
+  int step = min(n, k);
+  bool flag = true;
+
+  for (int i = 0; i < step; i++) {
+    int dis = b[cur];
+
+    if (dis > n) {
+      flag = false;
+      break;
+    }
+
+    cur = (cur - dis + n) % n;
+  }
+
+  if (flag)
+    cout << "YES" << "\n";
+  else
+    cout << "NO" << "\n";
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
