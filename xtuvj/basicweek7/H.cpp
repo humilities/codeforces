01f744c1d0c36e326dd2aa06a2a3c8836df7f60f
humilities <2099387470@qq.com>
2026-05-18 16:55:53 +0800

    wyqqqqqyw

--- xtuvj/basicweek7/H.cpp
+++ xtuvj/basicweek7/H.cpp
@@ -4,7 +4,43 @@
 using namespace std;
 using ll = long long;
 
-void solve() {}
+void solve() {
+  int n, c, q;
+  cin >> n >> c >> q;
+
+  ll len = n;
+  vector<ll> st(c);
+  vector<ll> ed(c);
+
+  vector<ll> oi(c);
+  string s;
+  cin >> s;
+
+  lin(i, 0, c) {
+    ll l, r;
+    cin >> l >> r;
+
+    st[i] = len + 1;
+    ed[i] = len + (r - l + 1);
+    oi[i] = l;
+
+    len = ed[i];
+  }
+
+  while (q--) {
+    ll k;
+    cin >> k;
+
+    for (int i = c - 1; i >= 0; i--) {
+      if (k >= st[i] && k <= ed[i]) {
+        ll of = k - st[i];
+        k = oi[i] + of;
+      }
+    }
+
+    cout << s[k - 1] << "\n";
+  }
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
