15afa215bf89f64289c07bee8ca0060409890ec3
humilities <2099387470@qq.com>
2026-05-18 14:01:05 +0800

    wyqqqqqyw

--- xtuvj/basicweek7/F.cpp
+++ xtuvj/basicweek7/F.cpp
@@ -10,6 +10,23 @@ void solve() {
 
   vector<int> a(n);
   lin(i, 0, n) cin >> a[i];
+
+  int c1 = 1;
+  int c2 = 0;
+
+  int flag = 1;
+  for (int i = 0; i < n; i++) {
+    if (a[i] < 0)
+      flag *= -1;
+
+    if (flag > 0)
+      c1++;
+    else
+      c2++;
+  }
+
+  cout << 1LL * c1 * c2 << " "
+       << 1LL * c1 * (c1 - 1) / 2 + 1LL * c2 * (c2 - 1) / 2;
 }
 
 int main() {
