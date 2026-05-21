fc54137f0ebb22bd2c4b9209be6469f421376fa8
humilities <2099387470@qq.com>
2026-05-21 15:43:59 +0800

    wyqqqqqyw

--- xtuvj/basicweek7/S.cpp
+++ xtuvj/basicweek7/S.cpp
@@ -4,12 +4,42 @@
 using namespace std;
 using ll = long long;
 
+const int mn = 100005;
+int f[mn];
+
 void solve() {
   int n;
   cin >> n;
 
   vector<int> a(n);
   lin(i, 0, n) cin >> a[i];
+
+  int ans = 1;
+  lin(i, 0, n) {
+    int temp = a[i];
+    vector<int> pri;
+
+    for (int j = 2; j * j <= temp; j++) {
+      if (temp % j == 0)
+        pri.push_back(j);
+      while (temp % j == 0)
+        temp /= j;
+    }
+    if (temp > 1)
+      pri.push_back(temp);
+
+    int len = 0;
+    for (auto k : pri)
+      len = max(len, f[k]);
+
+    len++;
+    for (auto k : pri)
+      f[k] = len;
+
+    ans = max(ans, len);
+  }
+
+  cout << ans;
 }
 
 int main() {
