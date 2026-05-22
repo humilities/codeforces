aa403866db558a9fcfbbe85fad9c74a56ee981e0
humilities <2099387470@qq.com>
2026-05-22 10:13:08 +0800

    wyqqqqqyw

---
+++ round/div2/1099div2/D.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+  int n;
+  cin >> n;
+
+  string s;
+  cin >> s;
+
+  vector<int> a(n);
+  vector<int> c(n);
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+  for (int i = 0; i < n; i++)
+    cin >> c[i];
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin >> t;
+
+  while (t--)
+    solve();
+
+  return 0;
+}
--- themeround/15/mixed/2/A2.cpp
+++ themeround/15/mixed/2/A2.cpp
@@ -4,23 +4,7 @@
 using namespace std;
 using ll = long long;
 
-void solve() {
-  int n;
-  cin >> n;
-
-  string ans;
-  char ba[] = {'a', 'e', 'i', 'o', 'u'};
-
-  int i = n / 5;
-  int j = n % 5;
-
-  for (int k = 0; k < 5; k++) {
-    int cnt = i + (k < j ? 1 : 0);
-    ans.append(cnt, ba[k]);
-  }
-
-  cout << ans << "\n";
-}
+void solve() {}
 
 int main() {
   ios_base::sync_with_stdio(false);
