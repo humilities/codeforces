93f6a73a9745fc65be6456f1234183c20fa4211c
humilities <2099387470@qq.com>
2026-05-29 21:06:32 +0800

    wyqqqqqyw

--- xtuvj/basicweek8/B.cpp
+++ xtuvj/basicweek8/B.cpp
@@ -3,7 +3,37 @@
 using namespace std;
 using ll = long long;
 
-void solve() {}
+void solve() {
+  int n;
+  cin >> n;
+
+  vector<int> a(n);
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+
+  map<int, int> ma;
+  for (int i = 0; i < n; i++)
+    ma[a[i]]++;
+
+  vector<int> fre;
+  for (auto [val, cnt] : ma)
+    fre.push_back(cnt);
+
+  sort(fre.begin(), fre.end());
+  int ans = 0;
+
+  // for (int i = 0; i < fre.size(); i++)
+  // cout << "fre[i]=" << " " << fre[i] << "\n";
+
+  for (int i = 0; i < fre.size(); i++) {
+    int val = fre[i];
+    int num = (fre.size() - i) * val;
+
+    ans = max(ans, num);
+  }
+
+  cout << n - ans << "\n";
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
