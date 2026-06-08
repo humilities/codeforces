13217f0bdbc2ed75a53adf6419cf8eb45f95b7af
humilities <2099387470@qq.com>
2026-06-08 15:37:44 +0800

    wyqqqqqyw

--- themeround/16/1/B.cpp
+++ themeround/16/1/B.cpp
@@ -10,6 +10,20 @@ void solve() {
   vector<int> a(n);
   for (int i = 0; i < n; i++)
     cin >> a[i];
+
+  int ans = a[0];
+  int cur = a[0];
+
+  for (int i = 1; i < n; i++) {
+    if ((a[i] & 1) != (a[i - 1] & 1))
+      cur += a[i];
+    else
+      cur = a[i];
+
+    ans = max(ans, cur);
+  }
+
+  cout << ans << "\n";
 }
 
 int main() {
