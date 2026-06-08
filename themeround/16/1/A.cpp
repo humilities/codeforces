7e38cd3655ad90cb023e0cab480beb8e5955d757
humilities <2099387470@qq.com>
2026-06-08 15:24:11 +0800

    wyqqqqqyw

--- themeround/16/1/A.cpp
+++ themeround/16/1/A.cpp
@@ -3,7 +3,43 @@
 using namespace std;
 using ll = long long;
 
-void solve() {}
+int luc(int n) {
+  int mm = 0, mn = 9;
+
+  while (n) {
+    int val = n % 10;
+    mm = max(mm, val);
+    mn = min(mn, val);
+
+    n /= 10;
+  }
+
+  return mm - mn;
+}
+
+void solve() {
+  int l, r;
+  cin >> l >> r;
+
+  if (r - l > 100) {
+    int k = (l / 100) * 100 + 90;
+    cout << k << "\n";
+  } else {
+    int na = 0;
+    int ans = 0;
+
+    for (int i = l; i <= r; i++) {
+      int num = luc(i);
+
+      if (num > na) {
+        na = num;
+        ans = i;
+      }
+    }
+
+    cout << ans << "\n";
+  }
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
