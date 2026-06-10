9e6c5a40fe8662b62b46328c141b515c288caec3
humilities <2099387470@qq.com>
2026-06-10 11:25:41 +0800

    wyqqqqqyw

--- themeround/16/2/A.cpp
+++ themeround/16/2/A.cpp
@@ -3,7 +3,28 @@
 using namespace std;
 using ll = long long;
 
-void solve() {}
+void solve() {
+  int n, k;
+  cin >> n >> k;
+
+  string s;
+  cin >> s;
+
+  int c = 0;
+  for (auto ch : s)
+    if (ch == '0')
+      c++;
+
+  int tar = k + c - n / 2;
+  if (!(tar & 1) && tar >= 0) {
+    int x = tar / 2;
+    if (x <= k) {
+      cout << "Yes" << "\n";
+      return;
+    }
+  }
+  cout << "No" << "\n";
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
