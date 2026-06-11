9d7c24bb861ffbcbcfd5ad8454be31eda326b6a1
humilities <2099387470@qq.com>
2026-06-11 17:04:55 +0800

    wyqqqqqyw

--- themeround/16/2/C.cpp
+++ themeround/16/2/C.cpp
@@ -3,7 +3,26 @@
 using namespace std;
 using ll = long long;
 
-void solve() {}
+void solve() {
+  int n, x;
+  cin >> n >> x;
+
+  vector<int> a(n);
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+
+  ll sum = 0;
+  int ma = 0;
+
+  for (int i = 0; i < n; i++) {
+    sum += a[i];
+    ma = max(ma, a[i]);
+  }
+
+  sum = (sum + x - 1) / x;
+
+  cout << max((ll)ma, sum) << "\n";
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
--- themeround/16/2/D.cpp
+++ themeround/16/2/D.cpp
@@ -3,7 +3,42 @@
 using namespace std;
 using ll = long long;
 
-void solve() {}
+void solve() {
+  int l, r;
+  cin >> l >> r;
+
+  vector<int> ans(r + 1, 0);
+  ll sum = 0;
+  ll curr = r;
+
+  while (curr > 0) {
+    ll mask = 1;
+    while (mask < curr)
+      mask = (mask << 1) | 1;
+
+    ll l = mask - curr;
+    ll le = l, rr = curr;
+
+    while (le <= rr) {
+      ans[le] = rr;
+      ans[rr] = le;
+
+      if (le == rr)
+        sum += (le | rr);
+      else
+        sum += (le | rr) * 2;
+
+      le++;
+      rr--;
+    }
+
+    curr = l - 1;
+  }
+
+  cout << sum << "\n";
+  for (int i = 0; i <= r; i++)
+    cout << ans[i] << " \n"[i == r];
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
@@ -17,3 +52,7 @@ int main() {
 
   return 0;
 }
+
+PP / ;.stdk[MMM]l,]]]]]]]]]]]]]]]]]]]
+ a
+  )----o
