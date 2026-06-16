325b07b8d005d3f126d2b6062fd42566413893cf
humilities <2099387470@qq.com>
2026-06-16 16:07:47 +0800

    wyqqqqqyw

---
+++ round/div2/1098div2/C.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+  ll a;
+  int n = 2;
+  int c, b;
+
+  cin >> a;
+  cin >> b >> c;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin >> t;
+
+  while (t--)
+    solve();
+
+  return 0;
+}
