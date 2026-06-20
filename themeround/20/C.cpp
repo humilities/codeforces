97509638374653ec121a092d36a0dbfad3203f1d
humilities <2099387470@qq.com>
2026-06-20 21:37:42 +0800

    wyqqqqqyw

--- themeround/20/C.cpp
+++ themeround/20/C.cpp
@@ -3,7 +3,33 @@
 using namespace std;
 using ll = long long;
 
-void solve() {}
+void solve() {
+  ll n, m;
+  cin >> n >> m;
+
+  n %= m;
+  if (n == 0) {
+    cout << 0 << "\n";
+    return;
+  }
+
+  ll ans = 0;
+  ll g = gcd(n, m);
+  ll m1 = m / g;
+
+  if ((m1 & (m1 - 1)) != 0) {
+    cout << -1 << "\n";
+    return;
+  }
+
+  while (n) {
+    ans += n;
+    n *= 2;
+    n %= m;
+  }
+
+  cout << ans << "\n";
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
