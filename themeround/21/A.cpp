500fbcbee7fb8ceb14fc0621b2b0a23a2e36540b
humilities <2099387470@qq.com>
2026-06-21 12:44:50 +0800

    wyqqqqqyw

--- themeround/21/A.cpp
+++ themeround/21/A.cpp
@@ -3,7 +3,27 @@
 using namespace std;
 using ll = long long;
 
-void solve() {}
+void solve() {
+  int n;
+  cin >> n;
+
+  string s;
+  cin >> s;
+
+  vector<int> one;
+  for (int i = 0; i < n; i++) {
+    if (s[i] == '1')
+      one.push_back(i);
+  }
+
+  int m = one.size();
+  int ans = 0;
+  for (int i = 0; i < m - 1; i++)
+    ans = max(ans, one[i + 1] - one[i] - 1);
+
+  int las = one[0] + (n - 1 - one[m - 1]);
+  cout << max(ans, las) << "\n";
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
