0f7565c02611fa168fff7e30eb5d050f25d846a6
humilities <2099387470@qq.com>
2026-07-19 23:18:39 +0800

    wyqqqqqyw

--- round/div2/1108div2/C.cpp
+++ round/div2/1108div2/C.cpp
@@ -5,13 +5,66 @@ using ll = long long;
 
 const int mod = 1e9 + 7;
 
+ll power(ll base, ll exp) {
+  ll ans = 1;
+  base %= mod;
+
+  while (exp) {
+    if (exp & 1)
+      ans = (ans * base) % mod;
+    base = (base * base) % mod;
+
+    exp >>= 1;
+  }
+
+  return ans;
+}
+
 void solve() {
   int n;
   cin >> n;
 
-  vector<int> a(n);
-  for (int i = 0; i < n; i++)
-    cin >> a[i];
+  int neg1 = 0;
+  vector<int> pos;
+  for (int i = 0; i < n; i++) {
+    int val;
+    cin >> val;
+
+    if (val > 0)
+      pos.push_back(val);
+    else
+      neg1++;
+  }
+
+  ll ans0 = 0;
+  ll ans1 = 0;
+
+  if (neg1 == 0) {
+    ans0 = 1;
+    ans1 = 0;
+  } else {
+    ll way = power(2, neg1 - 1);
+
+    ans0 = way;
+    ans1 = way;
+  }
+
+  map<int, ll> dppl;
+  map<int, ll> dpmi;
+  for (int val : pos) {
+    ll pre0 = ans0;
+    ll pre1 = ans1;
+    ll prepl = dppl[val];
+    ll premi0 = dpmi[val];
+    ll premi1 = dpmi[val - 1];
+
+    ans0 = (pre0 + prepl + premi1) % mod;
+    ans1 = (pre1 + premi0) % mod;
+    dppl[val] = (dppl[val] + pre0) % mod;
+    dpmi[val] = (dpmi[val] + pre1) % mod;
+  }
+
+  cout << ans0 << "\n";
 }
 
 int main() {
