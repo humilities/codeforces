75092dd065ca8af63a48cbd2ad7fb6bf5e2f13a8
humilities <2099387470@qq.com>
2026-07-21 15:54:40 +0800

    wyqqqqqyw

--- themeround/23/2/A.cpp
+++ themeround/23/2/A.cpp
@@ -3,7 +3,36 @@
 using namespace std;
 using ll = long long;
 
-void solve() {}
+void solve() {
+  int n;
+  cin >> n;
+
+  vector<int> b(n - 1);
+  for (int i = 0; i < n - 1; i++)
+    cin >> b[i];
+
+  vector<int> ans(n);
+
+  ans[0] = b[0];
+  ans[n - 1] = b[n - 2];
+
+  for (int i = 1; i < n - 1; i++)
+    ans[i] = b[i - 1] | b[i];
+
+  bool ok = true;
+  for (int i = 0; i < n - 1; i++) {
+    if ((ans[i] & ans[i + 1]) != b[i]) {
+      ok = false;
+      break;
+    }
+  }
+
+  if (!ok)
+    cout << -1 << "\n";
+  else
+    for (int i = 0; i < n; i++)
+      cout << ans[i] << " \n"[i == n - 1];
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
--- themeround/23/2/B.cpp
+++ themeround/23/2/B.cpp
@@ -3,7 +3,31 @@
 using namespace std;
 using ll = long long;
 
-void solve() {}
+void solve() {
+  int n;
+  ll sum = 0;
+  cin >> n;
+
+  vector<ll> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    sum += a[i];
+  }
+
+  vector<ll> pre(n + 1, 0);
+  for (int i = 0; i < n; i++)
+    pre[i + 1] = pre[i] + a[i];
+
+  ll mn = 0;
+  ll mi = 0;
+  for (int r = 1; r <= n; r++) {
+    ll i = (ll)r * r + r - pre[r];
+    mn = max(mn, i - mi);
+    mi = min(mi, i);
+  }
+
+  cout << sum + mn << "\n";
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
