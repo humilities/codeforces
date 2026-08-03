d4000f26e9e2db1d028493081ae3df6723ccdb85
humilities <2099387470@qq.com>
2026-08-03 20:37:27 +0800

    linziiiii

--- themeround/26/B.cpp
+++ themeround/26/B.cpp
@@ -8,24 +8,22 @@ void solve() {
   cin >> n;
 
   vector<ll> a(n);
-  ll max_val = -1e18; // 记录全局最大值
-  bool has_positive = false;
+  ll ma = -1e18;
+  bool has = false;
 
   for (int i = 0; i < n; i++) {
     cin >> a[i];
-    max_val = max(max_val, a[i]);
+    ma = max(ma, a[i]);
     if (a[i] > 0) {
-      has_positive = true;
+      has = true;
     }
   }
 
-  // 特判：如果全是负数，直接输出最大值
-  if (!has_positive) {
-    cout << max_val << "\n";
+  if (!has) {
+    cout << ma << "\n";
     return;
   }
 
-  // 累加奇偶位置的正数值（必须用 long long）
   ll n1 = 0, n2 = 0;
   for (int i = 0; i < n; i++) {
     if (i & 1)
