8379b94841c03e471eb5e234924be783a924d60a
humilities <2099387470@qq.com>
2026-08-03 17:36:22 +0800

    linziiiii

--- themeround/26/B.cpp
+++ themeround/26/B.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 using ll = long long;
 
-void solve() {}
+void solve() {
+  int n;
+  cin >> n;
+
+  vector<int> a(n);
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
